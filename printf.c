0753209f6efc7f660fb8c8f1b897ac061d763f72
malisapana98 <141355388+malisapana98@users.noreply.github.com>
2023-08-10 15:28:18 +0530

    Add files via upload

---
+++ printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+	int ;
+	printf("my name............sapana\n");
+    printf("my birth date......31/10/98\n");
+    printf("my age..............24\n");
+    printf("my address..........at vadodra\n");
+//	
+    
+	
+	
+}
